29c2c2ad4033fded29ea00797f5694193b33f669
sfranco1975 <sfranco1975@icloud.com>
2024-10-02 21:01:54 -0300

    first commit

---
+++ app/admin/page.tsx
@@ -0,0 +1,123 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import React from 'react'
+import StatCard from '@/components/StatCard'
+import { getRecentAppointmentList } from '@/lib/actions/appointment.actions'
+import { DataTable } from '@/components/table/DataTable'
+import { columns } from '@/components/table/columns'
+
+// async function getData(): Promise<Payment[]> {
+//   // Fetch data from your API here.
+//   return [
+//     {
+//       id: "728ed52f",
+//       amount: 100,
+//       status: "pending",
+//       email: "m@example.com",
+//     },
+//     {
+//       id: "728ed52f",
+//       amount: 100,
+//       status: "pending",
+//       email: "m@example.com",
+//     },
+//     {
+//       id: "728ed52f",
+//       amount: 100,
+//       status: "pending",
+//       email: "m@example.com",
+//     },
+//     {
+//       id: "728ed52f",
+//       amount: 100,
+//       status: "pending",
+//       email: "m@example.com",
+//     },
+//     {
+//       id: "728ed52f",
+//       amount: 100,
+//       status: "pending",
+//       email: "m@example.com",
+//     },
+//     {
+//       id: "728ed52f",
+//       amount: 100,
+//       status: "pending",
+//       email: "m@example.com",
+//     },
+//     {
+//       id: "728ed52f",
+//       amount: 100,
+//       status: "pending",
+//       email: "m@example.com",
+//     },
+//     {
+//       id: "728ed52f",
+//       amount: 100,
+//       status: "pending",
+//       email: "m@example.com",
+//     },
+//     // ...
+//   ]
+// }
+
+const Admin = async () => {
+  // const data = await getData()
+  const appointments = await getRecentAppointmentList()
+
+  console.log("ADMIN")
+  
+  return (
+    <div className='mx-auto flex max-w-7xl flex-col space-y-14'>
+      <header className='admin-header'>
+        <Link href="/" className='cursor-pointer'>
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={32}
+            width={162}
+            alt="logo"
+            className="h-8 w-fit"
+          />
+        </Link>
+
+        <p>Admin Dashboard</p>
+      </header>
+
+      <main className="admin-main">
+        <section className="w-full space-y-4">
+          <h1 className="header">Welcome 👋</h1>
+          <p className="text-dark-700">Start the day with managing new appointments.</p>
+        </section>
+
+        <section className="admin-stat">
+          <StatCard
+            type="appointments"
+            count={appointments.scheduledCount}
+            label="Scheduled appointments"
+            icon="/assets/icons/appointments.svg"
+          />
+          <StatCard
+            type="pending"
+            count={appointments.pendingCount}
+            label="Pending appointments"
+            icon="/assets/icons/pending.svg"
+          />
+          <StatCard
+            type="appointments"
+            count={appointments.cancelledCount}
+            label="Cancelled appointments"
+            icon="/assets/icons/cancelled.svg"
+          />
+        </section>
+
+        <DataTable columns={columns} data={appointments.documents} />       
+
+      </main>
+
+
+
+    </div>
+  )
+}
+
+export default Admin
---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://d852e17a3260425b933e70e5d12c0eb6@o4507727296004096.ingest.us.sentry.io/4507727301836800",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});
